eed876e7ff034cfab07ad482506d671cc39974b8
Jihye Jegal <jihyejegal@Jihyeui-MacBook-Air.local>
2017-05-04 17:46:20 +0900

    make Xcode Project file

---
+++ BnS_Info/BnS_Info/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BnS_Info
+//
+//  Created by Jihye Jegal on 2017. 5. 4..
+//  Copyright © 2017년 Jihye Jegal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
