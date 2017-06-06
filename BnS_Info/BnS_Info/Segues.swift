b85c746d95741f4175b2b6c0ae48e9531e87c029
표영권 <y007com@naver.com>
2017-06-06 22:58:39 +0900

    UI update.

--- BnS_Info/BnS_Info/Segues.swift
+++ BnS_Info/BnS_Info/Segues.swift
@@ -76,3 +76,55 @@ class Unwind_left: UIStoryboardSegue {
     }
     
 }
+
+// RightViewController  뷰 이동
+class right: UIStoryboardSegue {
+    
+    override func perform()
+    {
+        let src = self.source as UIViewController
+        let dst = self.destination as UIViewController
+        
+        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
+        dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width, y: 0)
+        
+        UIView.animate(withDuration: 0.25,
+                                   delay: 0.0,
+                                   options: UIViewAnimationOptions.curveEaseInOut,
+                                   animations: {
+                                    dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+                                    src.view.transform = CGAffineTransform(translationX: -src.view.frame.size.width/3, y: 0)
+        },
+                                   completion: { finished in
+                                    src.present(dst, animated: false, completion: nil)
+        }
+        )
+    }
+}
+// RightViewController  뷰 닫기
+class Unwind_right: UIStoryboardSegue {
+    
+    
+    override func perform()
+    {
+        let src = self.source as UIViewController
+        let dst = self.destination as UIViewController
+        
+        src.view.superview?.insertSubview(dst.view, belowSubview: src.view)
+        src.view.transform = CGAffineTransform(translationX: 0, y: 0)
+        dst.view.transform = CGAffineTransform(translationX: -src.view.frame.size.width/3, y: 0)
+        
+        UIView.animate(withDuration: 0.25,
+                                   delay: 0.0,
+                                   options: UIViewAnimationOptions.curveEaseInOut,
+                                   animations: {
+                                    src.view.transform = CGAffineTransform(translationX: src.view.frame.size.width, y: 0)
+                                    dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+        },
+                                   completion: { finished in
+                                    src.dismiss(animated: false, completion: nil)
+        }
+        )
+    }
+    
+}
