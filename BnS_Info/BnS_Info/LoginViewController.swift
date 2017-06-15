3c23f617f7bb4fa87ec46ea48503d9a29f077bd4
jkjh0819 <jkjh0819@naver.com>
2017-06-15 14:15:29 +0900

    add alert server connectino check

--- BnS_Info/BnS_Info/LoginViewController.swift
+++ BnS_Info/BnS_Info/LoginViewController.swift
@@ -7,29 +7,46 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var characterName: UITextField!
+    var connected:Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func connectionCheck(completion:@escaping (_ result:Bool) -> Void) {
+        Alamofire.request("http://127.0.0.1:8000/", method: .post, parameters: ["check":"check"], encoding: JSONEncoding.default, headers: nil).responseString { response in
+            print(response.result.value)
+            if response.result.value != nil {
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
     }
-     */
+    
+    
+    @IBAction func login(_ sender: Any) {
+        Alamofire.request("http://127.0.0.1:8000/", method: .post, parameters: ["check":"check"], encoding: JSONEncoding.default, headers: nil).responseString { response in
+            switch (response.result) {
+            case .success(_):
+                self.performSegue(withIdentifier: "unwindLogin", sender: self)
+                break
+            case.failure(_):
+                var alert = UIAlertView(title: "No Server Connection", message: "Please email to jkjh0819@naver.com", delegate: nil, cancelButtonTitle: "OK")
+                alert.show()
+                break
+            }
+        }
+
+    }
 }
